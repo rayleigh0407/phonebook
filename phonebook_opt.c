f865d8f946aacc2b86f825fd655d9214e7b2e995
rayleigh0407 <sasakikojilou@gmail.com>
2017-02-27 00:31:43 +0800

    Test 1:     phonebook_opt.h:         Create a new struct type "otherData":             put all information data of "entry" in it EXCEPT             "lastName"         Change struct "entry":             delete all information data EXCEPT "lastName"             create a "otherData" type pointer     phonebook_opt.c:         Change in function append():             allocate memory to e->data

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "phonebook_opt.h"
 
-/* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    e->data = (otherData *) malloc(sizeof(otherData));
+    strcpy(e->lastName, lastName);
+    /* put other data after this line */
+    e->pNext = NULL;
+
+    return e;
 }
