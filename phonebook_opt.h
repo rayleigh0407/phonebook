4199c48ca35691ff3f204c56b1afe9a674cf4bb1
rayleigh0407 <sasakikojilou@gmail.com>
2017-02-27 00:31:43 +0800

    Test 1: Modify the struct "entry" to decrease cache-miss

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,8 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+typedef struct __PHONE_BOOK_OTHER_DATA {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +16,11 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} otherData;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    otherData *data;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
